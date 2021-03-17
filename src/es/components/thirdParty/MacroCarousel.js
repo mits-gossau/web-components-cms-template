e973e9d382dd19ea5d96ffb91f95e5985de029ce
Silvan Strübi <silvan.struebi@mgb.ch>
2021-03-17 16:55:42 +0100

    documentation css added vars fixes attributes

--- src/es/components/thirdParty/MacroCarousel.js
+++ src/es/components/thirdParty/MacroCarousel.js
@@ -7,8 +7,37 @@ import { Shadow } from '../prototypes/Shadow.js'
  * Dependencies: https://github.com/ciampo/macro-carousel
  * Example at: /src/es/components/organisms/Slider.html
  *
- * @export
- * @class Slider
+ * @attribute {
+ *  {number + px} height
+ *  {number + px} [mobile-breakpoint=1000px]
+ *  {number} interval (autoplay is activated if this is set)
+ *  ↓↓↓ macro-carousel attributes ↓↓↓
+ *  https://github.com/ciampo/macro-carousel/blob/master/docs/macro-carousel.md#properties-all-reflected-to-attributes-in-kebab-case
+ *  {number} [selected=0]
+ *  {boolean} [loop=false]
+ *  {boolean} [navigation=false]
+ *  {boolean} [pagination=false]
+ * }
+ * @css {
+ *  --content-width [100%]
+ *  --margin [0 auto]
+ *  --outline-focus [0]
+ *  --content-width-mobile [100%]
+ *  --margin-mobile [0 auto]
+ *  --transition-duration [0.5s]
+ *  --pagination-position
+ *  --pagination-bottom
+ *  --pagination-background-color [black]
+ *  --pagination-background-color-selected [pink]
+ *  --pagination-width [5px]
+ *  --navigation-color [black]
+ *  --navigation-color-focus [black]
+ *  --navigation-background-color [transparent]
+ *  --navigation-background-color-focus [rgba(0, 0, 0, 0.2)]
+ *  --navigation-button-size [48px]
+ *  --navigation-icon-size [24px]
+ *  --navigation-icon-mask [url("data:image/svg+xml;charset=utf-8,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='%23000'%3E %3Cpath d='M15.41 7.41L14 6l-6 6 6 6 1.41-1.41L10.83 12z'/%3E %3C/svg%3E")]
+ * }
  * @type {CustomElementConstructor}
  */
 export default class Slider extends Shadow() {
@@ -16,14 +45,16 @@ export default class Slider extends Shadow() {
     super(...args)
 
     this.macroCarousel = document.createElement('macro-carousel')
-    Array.from(this.attributes).forEach(attribute => {
-      if (attribute.name) this.macroCarousel.setAttribute(attribute.name, attribute.key || 'true')
-    })
+    // copy all kids into the macro-carousel
     Array.from(this.root.children).forEach(node => {
       if (!node.getAttribute('slot') && node.tagName !== 'STYLE') {
         node.setAttribute('loading', 'eager') // must be eager, not that it loads once visible
         this.macroCarousel.appendChild(node)
       }
+    })
+    // forward all attributes
+    Array.from(this.attributes).forEach(attribute => {
+      if (attribute.name) this.macroCarousel.setAttribute(attribute.name, attribute.value || 'true')
     })
     this.interval = null
   }
@@ -62,6 +93,8 @@ export default class Slider extends Shadow() {
         --height: auto;
         --width: auto;
         --width-mobile: auto;
+        width: var(--content-width, 100%);
+        margin: var(--margin, 0 auto);
       }
       :host > macro-carousel > * {
         --img-height: ${this.getAttribute('height') ? 'min(auto, 100%)' : 'auto'};
@@ -77,6 +110,12 @@ export default class Slider extends Shadow() {
       :host > macro-carousel *:focus {
         outline: var(--outline-focus, 0);
       }
+      @media only screen and (max-width: ${this.getAttribute('mobile-breakpoint') ? this.getAttribute('mobile-breakpoint') : self.Environment && !!self.Environment.mobileBreakpoint ? self.Environment.mobileBreakpoint : '1000px'}) {
+        :host > macro-carousel {
+          width: var(--content-width-mobile, 100%);
+          margin: var(--margin-mobile, 0 auto);
+        }
+      }
     `
     // inject style which can't be controlled through css vars
     // style which must be inside macro-carousel shadowDom
@@ -84,16 +123,25 @@ export default class Slider extends Shadow() {
     // get more from here: https://github.com/ciampo/macro-carousel/blob/master/src/macro-carousel/macro-carousel.css
     this.injectStyle.innerHTML = /* css */`
       :host {
-        --macro-carousel-transition-duration: var(--transition-duration);
+        --macro-carousel-transition-duration: var(--transition-duration, 0.5s);
       }
       :host > #pagination {
         position: var(--pagination-position);
         bottom: var(--pagination-bottom);
       }
       :host div ::slotted(macro-carousel-pagination-indicator) {
-        --macro-carousel-pagination-color: var(--pagination-background-color);
-        --macro-carousel-pagination-color-selected: var(--pagination-background-color-selected);
-        --macro-carousel-pagination-size-dot: var(--pagination-width);
+        --macro-carousel-pagination-color: var(--pagination-background-color, black);
+        --macro-carousel-pagination-color-selected: var(--pagination-background-color-selected, pink);
+        --macro-carousel-pagination-size-dot: var(--pagination-width, 5px);
+      }
+      :host div ::slotted(macro-carousel-nav-button) {
+        --macro-carousel-navigation-color: var(--navigation-color, black);
+        --macro-carousel-navigation-color-focus: var(--navigation-color-focus, black);
+        --macro-carousel-navigation-color-background: var(--navigation-background-color, transparent);
+        --macro-carousel-navigation-color-background-focus: var(--navigation-background-color-focus, rgba(0, 0, 0, 0.2));
+        --macro-carousel-navigation-button-size: var(--navigation-button-size, 48px);
+        --macro-carousel-navigation-icon-size: var(--navigation-icon-size, 24px);
+        --macro-carousel-navigation-icon-mask: var(--navigation-icon-mask, url("data:image/svg+xml;charset=utf-8,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='%23000'%3E %3Cpath d='M15.41 7.41L14 6l-6 6 6 6 1.41-1.41L10.83 12z'/%3E %3C/svg%3E"));
       }
     `
   }
@@ -141,6 +189,7 @@ export default class Slider extends Shadow() {
   }
 
   macroCarouselReady () {
+    // style which has to be injected to take effect
     this.macroCarousel.shadowRoot.appendChild(this.injectStyle)
     // autoplay
     if (this.getAttribute('interval')) {
