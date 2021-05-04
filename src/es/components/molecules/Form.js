d6aee138ef211c4eef344a1aae1d4d275571f1ea
joeljxse <joeljosewattwil@gmail.com>
2021-05-04 15:31:59 +0200

    || operator in form

--- src/es/components/molecules/Form.js
+++ src/es/components/molecules/Form.js
@@ -228,8 +228,7 @@ export default class Form extends Shadow() {
     this.loadChildComponents().then(children => {
       Array.from(this.root.querySelectorAll('input'))
       .filter(i => i.getAttribute('type') != "hidden").forEach(input => {
-        const label = this.root.querySelector(`label[for=${input.getAttribute("name")}]`) 
-          ?? this.root.querySelector(`label[for=${input.getAttribute("id")}]`);
+        const label = this.root.querySelector(`label[for=${input.getAttribute("name")}]`) || this.root.querySelector(`label[for=${input.getAttribute("id")}]`);
         const aInput = new children[0][1](input, label, { namespace: this.getAttribute('namespace') || ''})
         aInput.setAttribute('type', input.getAttribute('type'))
         input.replaceWith(aInput)
