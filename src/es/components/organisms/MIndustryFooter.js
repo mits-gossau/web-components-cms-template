c161edc836bab99afc46bca036598a8d29a0b654
joeljxse <joeljosewattwil@gmail.com>
2021-06-29 16:32:17 +0200

    test mindustryfooter

---
+++ src/es/components/organisms/MIndustryFooter.js
@@ -0,0 +1,43 @@
+// @ts-check
+import { Footer } from '../web-components-cms-template/src/es/components/organisms/Footer.js'
+
+/* global self */
+/* global Link */
+/* global customElements */
+
+/**
+ * Footer is sticky and hosts uls
+ * Example at: /src/es/components/organisms/Playlist.html
+ * As an organism, this component shall hold molecules and/or atoms
+ *
+ * @export
+ * @class Footer
+ * @type {CustomElementConstructor}
+ * @attribute {
+ *  {string} [logo-load="logo-load"]
+ *  {boolean} [homepage] for classics homepage styles (only one logo at right side)
+ * }
+ * @css {
+ *  NOTE: grid-area: footer;
+ *  --background-color [black]
+ *  --z-index [100]
+ *  --content-spacing [40px]
+ *  --a-link-content-spacing [0]
+ *  --a-link-font-size [1rem]
+ *  --a-link-font-size-2 [1rem]
+ *  --list-style [none]
+ *  --align-items [start]
+ *  --font-size [1rem]
+ *  --p-margin [0]
+ * }
+ */
+export default class MIndustryFooter extends Footer() {
+  constructor (...args) {
+    super(...args)
+  }
+
+
+  connectedCallback = () => {
+    this.html = `<h1>test</h1>`
+  }
+}
