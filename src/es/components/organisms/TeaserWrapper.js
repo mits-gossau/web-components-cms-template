257b01a1b6593bdbcc01e0dfda3bed9a00e0a870
Weedshaker <weedshaker@gmail.com>
2021-10-29 23:43:41 +0200

    teaser extending body teaser wrapper removed resp. ext wrapper wrapper added as flexible flex component shadow fix

--- src/es/components/organisms/TeaserWrapper.js
+++ src/es/components/organisms/TeaserWrapper.js
@@ -1,5 +1,5 @@
 // @ts-check
-import { Shadow } from '../prototypes/Shadow.js'
+import {Wrapper} from './Wrapper.js'
 
 /**
  * As an organism, this component shall hold molecules and/or atoms
@@ -8,59 +8,7 @@ import { Shadow } from '../prototypes/Shadow.js'
  * @class TeaserWrapper
  * @type {CustomElementConstructor}
  * @css {
- *  --margin [20px]
- *  --min-width [370px]
- *  --justify-content [center]
+ *  
  * }
  */
-export default class TeaserWrapper extends Shadow() {
-  connectedCallback () {
-    if (this.shouldComponentRenderCSS()) this.renderCSS()
-  }
-
-  /**
-   * evaluates if a render is necessary
-   *
-   * @return {boolean}
-   */
-  shouldComponentRenderCSS () {
-    return !this.root.querySelector(`:host > style[_css], ${this.tagName} > style[_css]`)
-  }
-
-  /**
-   * renders the o-teaser-wrapper css
-   *
-   * @return {void}
-   */
-  renderCSS () {
-    const teaserAmount = this.root.querySelectorAll('m-teaser').length
-    this.css = /* css */`
-      :host {
-        width: 100%;
-        display: flex;
-        flex-wrap: wrap;
-        justify-content: var(--justify-content, center);
-      }
-      :host > * {
-        min-width: min(var(--min-width, 370px), 100%);
-        width: 100%;
-        margin: var(--margin, 20px);
-      }
-    `
-
-    if (teaserAmount % 2 === 0) {
-      this.css = /* css */`
-      :host > * {
-        max-width: calc(50% - 2 * var(--margin, 20px));
-      }
-      
-      `
-    } else {
-      this.css = /* css */`
-      :host > * {
-        max-width: calc(100% / 3 - 2 * var(--margin, 20px));
-      }
-      `
-    }
-  }
-}
+export default class TeaserWrapper extends Wrapper() {}
