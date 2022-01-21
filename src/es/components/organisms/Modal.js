fcc0722850f4082a39af900b3d090174c3942357
Weedshaker <weedshaker@gmail.com>
2022-01-21 18:50:22 +0100

    modal stuff

--- src/es/components/organisms/Modal.js
+++ src/es/components/organisms/Modal.js
@@ -75,6 +75,7 @@ export default class Modal extends Shadow() {
           this.setAttribute('open', '')
           this.clone.setAttribute('open', '')
         }
+        self.requestAnimationFrame(timeStamp => this.setContainerMaxWidth())
       }
     }
     // close
@@ -93,21 +94,25 @@ export default class Modal extends Shadow() {
           if (this.clone && (child = this.container.childNodes[0]) && this.clone !== child) this.clone.replaceWith(child)
           this.clone = null
           this.container.innerHTML = ''
+          this.style.textContent = ''
         }
       }
     }
+    this.clickListener = event => event.stopPropagation()
   }
 
   connectedCallback () {
     if (this.shouldComponentRenderCSS()) this.renderCSS()
     if (this.shouldComponentRenderHTML()) this.renderHTML();
     (this.getAttribute('listen-at') ? document.querySelector(this.getAttribute('listen-at')) : document.body).addEventListener(this.getAttribute('open-modal') || 'open-modal', this.openModalListener);
-    (this.closeBtn ? this.closeBtn : this).addEventListener('click', this.closeModalListener)
+    (this.hasAttribute('btn-close-only') && this.closeBtn ? this.closeBtn : this).addEventListener('click', this.closeModalListener)
+    this.addEventListener('click', this.clickListener)
   }
 
   disconnectedCallback () {
     (this.getAttribute('listen-at') ? document.querySelector(this.getAttribute('listen-at')) : document.body).removeEventListener(this.getAttribute('open-modal') || 'open-modal', this.openModalListener);
-    (this.closeBtn ? this.closeBtn : this).removeEventListener('click', this.closeModalListener)
+    (this.hasAttribute('btn-close-only') && this.closeBtn ? this.closeBtn : this).removeEventListener('click', this.closeModalListener)
+    this.removeEventListener('click', this.clickListener)
   }
 
   attributeChangedCallback (name, oldValue, newValue) {
@@ -156,7 +161,7 @@ export default class Modal extends Shadow() {
       }
       :host([open]) > section {
         align-items: center;
-        background-color: var(--background-color, rgba(0, 0, 0, 0.8));
+        background-color: var(--background-color, rgba(0, 0, 0, 0.85));
         box-sizing: border-box;
         display: flex;
         height: var(--height, 100%);
@@ -174,9 +179,21 @@ export default class Modal extends Shadow() {
         display: var(--display, flex);
         flex-direction: var(--flex-direction, row);
         justify-content: var(--justify-content, center);
+        max-height: var(--max-height, 100vh);
+        margin: var(--margin, 0 10px);
       }
       :host([open]) > section > div > #close {
+        display: var(--close-display, block);
+        position: var(--close-position, static);
+        top: var(--close-top, auto);
+        right: var(--close-right, auto);
+        bottom: var(--close-bottom, auto);
+        left: var(--close-left, auto);
         margin: var(--close-margin, 0 10px);
+      }
+      :host([open]) > section > div > #close > ${this.getAttribute('a-menu-icon') || 'a-menu-icon'}.open {
+        font-size: var(--a-font-size-open, var(--font-size-open, var(--a-font-size, var(--font-size))));
+        transform: translate(-0.9em, max(-8vw, -1.5em));
       }
       @media only screen and (max-width: ${this.getAttribute('mobile-breakpoint') ? this.getAttribute('mobile-breakpoint') : self.Environment && !!self.Environment.mobileBreakpoint ? self.Environment.mobileBreakpoint : '1000px'}) {
         :host([open]) > section {
@@ -210,5 +227,48 @@ export default class Modal extends Shadow() {
     this.section = document.createElement('section')
     this.section.appendChild(this.container = document.createElement('div'))
     this.html = this.section
+    this.html = this.style
+  }
+
+  setContainerMaxWidth () {
+    const margins = this.cleanPropertyMarginValue(self.getComputedStyle(this.container).getPropertyValue(`--${this.namespace || ''}margin`))
+    const height = `calc(100vh - ${margins[0]} - ${margins[2]})`;
+    this.style.textContent = /* css */`
+      :host([open]) > section > div {
+        ${this.containerNamespaces.reduce((acc, namespace) => acc + this.getMaxWidthString(namespace, height), `max-height: ${height};${this.getMaxWidthString('', height)}`)}
+      }
+    }`
+  }
+
+  getMaxWidthString (namespace = '', height = '100vh') {
+    return `--${namespace}img-max-height: ${height};--${namespace}max-height: ${height};`
+  }
+
+  /**
+   * @param {string | any} value
+   * @returns {number | false}
+   */
+  cleanPropertyMarginValue (value) {
+    if (!value) return false
+    let values = value.trimStart().split(' ').map(val => !val || val === '0' ? '0px' : val)
+    if (values.length === 0) return false
+    if (values.length === 1) values = Array(4).fill(values[0])
+    if (values.length === 2) values = [values[0], values[1], values[0], values[1]]
+    if (values.length === 3) values = [values[0], values[1], values[2], values[1]]
+    const valueRegex = /[^a-z%]/g
+    return values
+  }
+
+
+  get style () {
+    return this._style || (this._style = (() => {
+      const style = document.createElement('style')
+      style.setAttribute('protected', 'true')
+      return style
+    })())
+  }
+
+  get containerNamespaces () {
+    return Array.from(this.container.children).map(child => child.getAttribute('namespace') || '')
   }
 }
