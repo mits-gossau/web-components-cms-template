dd41ab8f1861c91094cb2f1c8cd301f344c388fa
Laurin-dev <laurin.graf@mgb.ch>
2022-02-01 15:31:42 +0100

    Carousel better custom pagination

--- src/es/components/thirdParty/MacroCarousel.js
+++ src/es/components/thirdParty/MacroCarousel.js
@@ -38,7 +38,7 @@ import { Shadow } from '../prototypes/Shadow.js'
  *  --pagination-bottom
  *  --pagination-background-color [black]
  *  --pagination-background-color-selected [pink]
- *  --pagination-width [5px]
+ *  --pagination-height [5px]
  *  --navigation-color [black]
  *  --navigation-color-focus [black]
  *  --navigation-background-color [transparent]
@@ -229,11 +229,15 @@ export default class MacroCarousel extends Shadow() {
       :host > #pagination {
         position: var(--pagination-position);
         bottom: var(--pagination-bottom);
+        --macro-carousel-pagination-height: calc(var(--pagination-height)*3);
       }
       :host div ::slotted(macro-carousel-pagination-indicator) {
         --macro-carousel-pagination-color: var(--pagination-background-color, var(--background-color, black));
         --macro-carousel-pagination-color-selected: var(--pagination-background-color-selected, var(--background-color-selected, var(--background-color, pink)));
-        --macro-carousel-pagination-size-dot: var(--pagination-width, 5px);
+        --macro-carousel-pagination-size-dot: var(--pagination-height, 5px);
+        --macro-carousel-pagination-size-dot-selected: 6rem;
+        --macro-carousel-pagination-border-selected: var(--pagination-border-selected);
+        --macro-carousel-pagination-size-clickable: calc(var(--pagination-width) * 2);
         opacity: var(--pagination-opacity, 1);
       }
       :host div ::slotted(macro-carousel-nav-button) {
@@ -244,6 +248,35 @@ export default class MacroCarousel extends Shadow() {
         --macro-carousel-navigation-button-size: var(--navigation-button-size, 48px);
         --macro-carousel-navigation-icon-size: var(--navigation-icon-size, 24px);
         --macro-carousel-navigation-icon-mask: var(--navigation-icon-mask, url("data:image/svg+xml;charset=utf-8,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='%23000'%3E %3Cpath d='M15.41 7.41L14 6l-6 6 6 6 1.41-1.41L10.83 12z'/%3E %3C/svg%3E"));
+      }
+    `.replace(/var\(--/g, `var(--${this.namespace}`)
+    
+    var rs = getComputedStyle(this.root.children[0])
+    var width = this.cleanPropertyWidthValue(rs.getPropertyValue(`--${this.namespace}pagination-width`))   
+    var height = this.cleanPropertyWidthValue(rs.getPropertyValue(`--${this.namespace}pagination-height`))
+    var ratio = width/height
+   
+    this.injectStylePagination = document.createElement('style')
+    this.injectStylePagination.innerHTML = /*css*/`
+      .fg {
+        width: calc(var(--pagination-width, 5px));
+        border-radius: var(--pagination-border-radius, 0.5rem);
+      }
+      .bg {
+        width: var(--pagination-width, 5px);
+        border-radius: var(--pagination-border-radius, 0.5rem);
+      }
+      :host(.selected) .fg{
+        width: calc(var(--pagination-width, 5px) * ${ratio});
+      }
+      :host(.selected) .bg {
+        width: calc(var(--pagination-width, 5px) * ${ratio*0.5 + 0.5});
+      }
+      :host(.selected) {
+        width: calc(var(--pagination-width) * ${ratio+1});
+      }
+      :host {
+        border-radius: var(--pagination-border-radius, 0.5rem);
       }
     `.replace(/var\(--/g, `var(--${this.namespace}`)
   }
@@ -291,8 +324,15 @@ export default class MacroCarousel extends Shadow() {
   }
 
   macroCarouselReady () {
-    // style which has to be injected to take effect
+    // style which has to be injected to take effect on Carousel
     this.macroCarousel.shadowRoot.appendChild(this.injectStyle)
+
+    // style which has to be injected to take effect on pagination-indicator
+    this.macroCarousel.querySelectorAll('macro-carousel-pagination-indicator').forEach(p => {
+      var dupNode = this.injectStylePagination.cloneNode(true);
+      p.shadowRoot.append(dupNode)
+    })
+
     // autoplay
     if (this.getAttribute('interval')) {
       this.setInterval()
