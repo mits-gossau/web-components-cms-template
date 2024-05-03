b734fe2098cab3c7d188d63f643e9b221821dfdb
rs-mgb <roland.szabo@mgb.ch>
2024-05-03 14:30:19 +0200

    Added separated-span wrap

--- src/es/components/atoms/Title.js
+++ src/es/components/atoms/Title.js
@@ -123,6 +123,8 @@ export default class Title extends Shadow() {
           this.h1.append(`${text} `)
         }
       })
+    } else if (this.getAttribute('wrap') === 'separated-span') {
+      return
     } else {
       this.textContent.split(' ').forEach((text, i, arr) => {
         const span = document.createElement('span')
